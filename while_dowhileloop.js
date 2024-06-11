f5c2fb971e0d040a5bb87d3a7f38a1188307e389
maadesh <151980884+Maadesh11@users.noreply.github.com>
2024-06-11 17:58:12 +0530

    Create while_dowhileloop.js

---
+++ while_dowhileloop.js
@@ -0,0 +1,15 @@
+let i=0;
+while(i<=20){
+    if(i%2!=0)
+        { document.write(i+"<br>");
+    
+}
+   i++;
+}
+document.write("do while will exceute the loop whether the condition is right or wrong"+"<br>");
+let j=0;
+do{
+    document.write(j+"<br>");
+    j++;
+}
+while(j<=10);
